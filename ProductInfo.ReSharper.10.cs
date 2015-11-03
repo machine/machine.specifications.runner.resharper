dd6ffb0099df5a4cc5ffb8c2972d305bd43f2e91
Roman Müller <roman.mueller@erowa.com>
2015-11-03 16:58:22 +0100

    Prepared resharper 10 support

--- ProductInfo.ReSharper.10.cs
+++ ProductInfo.ReSharper.10.cs
@@ -2,5 +2,5 @@
 {
     public const string Product = "ReSharper";
     public const string Uuid = "53632015-1978-4832-9FBA-4424B804D6AB";
-    public const string Version = "9.2";
+    public const string Version = "10";
 }
