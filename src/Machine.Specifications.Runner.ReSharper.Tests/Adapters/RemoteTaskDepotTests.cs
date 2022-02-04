2eee222f1b7a168383022cc91518900cade0be4c
Robert Coltheart <robert.coltheart@gmail.com>
2022-02-04 08:38:38 +0800

    tests

--- src/Machine.Specifications.Runner.ReSharper.Tests/Adapters/RemoteTaskDepotTests.cs
+++ src/Machine.Specifications.Runner.ReSharper.Tests/Adapters/RemoteTaskDepotTests.cs
@@ -12,23 +12,85 @@ namespace Machine.Specifications.Runner.ReSharper.Tests.Adapters
     public class RemoteTaskDepotTests
     {
         [Test]
-        public void CanGetTaskByElement()
+        public void CanGetContextByElement()
         {
-            var depot = new RemoteTaskDepot(GetTasks().ToArray());
+            var contextTask = MspecContextRemoteTask.ToServer("Namespace.Context", null, null, null);
+            var contextElement = new ContextElement("Namespace.Context", string.Empty);
 
-            foreach (var element in GetElements())
-            {
-                Assert.NotNull(depot[element]);
-            }
+            var depot = new RemoteTaskDepot(new RemoteTask[] { contextTask });
+
+            Assert.NotNull(depot[contextElement]);
+        }
+
+        [Test]
+        public void CanGetSpecificationByElement()
+        {
+            var contextTask = MspecContextRemoteTask.ToServer("Namespace.Context", null, null, null);
+            var specificationTask = MspecSpecificationRemoteTask.ToServer("Namespace.Context", "should", null, null, null, null);
+
+            var contextElement = new ContextElement("Namespace.Context", string.Empty);
+            var specificationElement = new SpecificationElement(contextElement, "should");
+
+            var depot = new RemoteTaskDepot(new RemoteTask[] { contextTask, specificationTask });
+
+            Assert.NotNull(depot[specificationElement]);
+        }
+
+        [Test]
+        public void CanGetBehaviorByElement()
+        {
+            var contextTask = MspecContextRemoteTask.ToServer("Namespace.Context", null, null, null);
+            var behaviorTask = MspecSpecificationRemoteTask.ToServer("Namespace.Context", "behaves_like", "Namespace.ABehavior", null, null, null);
+
+            var contextElement = new ContextElement("Namespace.Context", string.Empty);
+            var behaviorElement = new BehaviorElement(contextElement, "Namespace.ABehavior", "behaves_like");
+
+            var depot = new RemoteTaskDepot(new RemoteTask[] { contextTask, behaviorTask });
+
+            Assert.NotNull(depot[behaviorElement]);
+        }
+
+        [Test]
+        public void CanGetBehaviorSpecificationByElement()
+        {
+            var contextTask = MspecContextRemoteTask.ToServer("Namespace.Context", null, null, null);
+            var behaviorTask = MspecSpecificationRemoteTask.ToServer("Namespace.Context", "behaves_like", "Namespace.ABehavior", null, null, null);
+            var specificationTask = MspecBehaviorSpecificationRemoteTask.ToServer("Namespace.Context.behaves_like", "Namespace.Context", "should_behave", null);
+
+            var contextElement = new ContextElement("Namespace.Context", string.Empty);
+            var specificationElement = new SpecificationElement(contextElement, "should");
+            var behaviorElement = new BehaviorElement(contextElement, "Namespace.ABehavior", "behaves_like");
+            var behaviorSpecificationElement = new SpecificationElement(contextElement, "should_behave", behaviorElement);
+
+            var depot = new RemoteTaskDepot(new RemoteTask[] { contextTask, behaviorTask, specificationTask });
+
+            Assert.NotNull(depot[specificationElement]);
+        }
+
+        [Test]
+        public void CanGetBehaviorWhenThereIsSpecificationWithSameName()
+        {
+            var contextTask = MspecContextRemoteTask.ToServer("Namespace.Context", null, null, null);
+            var specificationTask = MspecSpecificationRemoteTask.ToServer("Namespace.Context", "should", null, null, null, null);
+            var behaviorTask = MspecSpecificationRemoteTask.ToServer("Namespace.Context", "behaves_like", "Namespace.ABehavior", null, null, null);
+            var behaviorSpecificationTask = MspecBehaviorSpecificationRemoteTask.ToServer("Namespace.Context.behaves_like", "Namespace.Context", "should_behave", null);
+
+            var contextElement = new ContextElement("Namespace.Context", string.Empty);
+            var behaviorElement = new BehaviorElement(contextElement, "Namespace.ABehavior", "behaves_like");
+            var specificationElement = new SpecificationElement(contextElement, "should_behave", behaviorElement);
+
+            var depot = new RemoteTaskDepot(new RemoteTask[] { contextTask, behaviorTask, specificationTask });
+
+            Assert.NotNull(depot[specificationElement]);
         }
 
         private IEnumerable<RemoteTask> GetTasks()
         {
             yield return MspecContextRemoteTask.ToServer("Namespace.Context", null, null, null);
             yield return MspecSpecificationRemoteTask.ToServer("Namespace.Context", "should", null, null, null, null);
-            yield return MspecBehaviorSpecificationRemoteTask.ToServer("Namespace.Context.behaves_like", "Namespace.Context", "behaves_like", null);
-            yield return MspecSpecificationRemoteTask.ToServer("Namespace.Context", "should_behave", "Namespace.ABehavior", null, null, null);
-            yield return MspecSpecificationRemoteTask.ToServer("Namespace.Context", "should_not_behave", "Namespace.ABehavior", null, null, null);
+            yield return MspecSpecificationRemoteTask.ToServer("Namespace.Context", "behaves_like", "Namespace.ABehavior", null, null, null);
+            yield return MspecBehaviorSpecificationRemoteTask.ToServer("Namespace.Context.behaves_like", "Namespace.Context", "should_behave", null);
+            yield return MspecBehaviorSpecificationRemoteTask.ToServer("Namespace.Context.behaves_like", "Namespace.Context", "should_not_behave", null);
         }
 
         private IEnumerable<IMspecElement> GetElements()
