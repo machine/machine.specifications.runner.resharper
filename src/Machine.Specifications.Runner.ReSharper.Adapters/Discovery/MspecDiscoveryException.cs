be857e6c12b7e80ff727574ca9386ebd2a8e1f6a
Robert Coltheart <13191652+robertcoltheart@users.noreply.github.com>
2022-05-04 18:06:18 +1000

    Catch exceptions that originate from the framework assembly (#216)

---
+++ src/Machine.Specifications.Runner.ReSharper.Adapters/Discovery/MspecDiscoveryException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Machine.Specifications.Runner.ReSharper.Adapters.Discovery
+{
+    [Serializable]
+    public class MspecDiscoveryException : Exception
+    {
+        public MspecDiscoveryException(string message, string stackTrace)
+            : base(message)
+        {
+            StackTrace = stackTrace;
+        }
+
+        protected MspecDiscoveryException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            StackTrace = info.GetString(nameof(StackTrace));
+        }
+
+        public override string StackTrace { get; }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue(nameof(StackTrace), StackTrace);
+        }
+    }
+}
